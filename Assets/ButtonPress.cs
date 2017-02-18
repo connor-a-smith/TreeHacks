6a5a713e971c34ad6e7fdfdd93d52d5a54c006b6
Connor Smith <Connor Smith>
2017-02-18 15:01:23 -0800

    Worked on room, added projector screens and trigger buttons

---
+++ Assets/ButtonPress.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour {
+
+    [SerializeField]
+    private OptionButton testButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            testButton.PressButton();            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+
+            testButton.ReleaseButton();
+
+
+        }
+		
+	}
+}

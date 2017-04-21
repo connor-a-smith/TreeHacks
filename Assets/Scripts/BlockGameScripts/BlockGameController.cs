aee2a31a021d9a14f769b5544739434f5f73435f
Ani Kan <Ani Kan>
2017-04-20 22:09:08 -0700

    Work on new block minigame. Functionality works, need to implement tv and grabbing

---
+++ Assets/Scripts/BlockGameScripts/BlockGameController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockGameController : MonoBehaviour {
+
+    private bool isTextActive;
+    private bool isVoiceActive;
+
+    private BlockColor desiredColor;
+    private BlockShape desiredShape;
+
+    private Text MonitorText;
+    private AudioSource speaker;
+
+    public Transform blockSpawnPosition;
+    public GameObject blockPrefab;
+
+    private GameObject[] blocks = new GameObject[3];
+	// Use this for initialization
+	void Start () {
+        StartNewRound();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartNewRound()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            //Destroy old round blocks.
+            Destroy(blocks[i]);
+            blocks[i] = GameObject.Instantiate(blockPrefab, blockSpawnPosition.position + new Vector3(0, 0, .5f * i), Quaternion.identity);
+            blocks[i].GetComponent<BlockProperties>().InitBlock();
+        }
+
+        int randomBlockIndex = Random.Range(0, blocks.Length);
+
+        desiredColor = blocks[randomBlockIndex].GetComponent<BlockProperties>().color;
+        
+        //All same color.
+        //All same shape.
+        //Only voice for instructions.
+        //Only text for instructions.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BlockProperties properties = other.gameObject.GetComponent<BlockProperties>();
+        if (properties != null)
+        {
+            //Win
+            if (properties.color == desiredColor)
+            {
+                GetComponentInChildren<ParticleSystem>().Play();
+
+            }
+
+            //Lose
+            else
+            {
+
+            }
+
+            StartCoroutine(StartNewRoundSoon());
+        }
+    }
+
+    public IEnumerator StartNewRoundSoon()
+    {
+        yield return new WaitForSeconds(2.0f);
+        StartNewRound();
+    }
+}
---
+++ Assets/Scripts/BlockGameScripts/BlockProperties.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockColor
+{
+    Green, Red, Brown, Blue, Purple, Yellow, Gray, Black
+};
+
+public enum BlockShape
+{
+    Cube, Sphere, Cylinder
+};
+
+public class BlockProperties : MonoBehaviour {
+    
+    public BlockColor color;
+    public BlockShape shape;
+
+    private static Shader standardShader;
+
+	// Use this for initialization
+	void Start () {
+        //Set the standard shader only once.
+        if (!standardShader)
+        {
+            standardShader = Shader.Find("Standard");
+        }
+    }
+
+    public void InitBlock()
+    {
+        RandomizeColor();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void RandomizeColor()
+    {
+        //Pick a random enum.
+        color = (BlockColor) Random.Range(0, System.Enum.GetNames(typeof(BlockColor)).Length);
+
+        Color matColor;
+
+        //Set the color.
+        switch (color)
+        {
+            case BlockColor.Green:
+                matColor = Color.green;
+                break;
+            case BlockColor.Red:
+                matColor = Color.red;
+                break;
+            case BlockColor.Brown:
+                matColor = new Color(.71f, .396f, .114f);
+                break;
+            case BlockColor.Blue:
+                matColor = Color.blue;
+                break;
+            case BlockColor.Purple:
+                matColor = new Color(.33f, .1f, 0.54f);
+                break;
+            case BlockColor.Yellow:
+                matColor = Color.yellow;
+                break;
+            case BlockColor.Gray:
+                matColor = Color.gray;
+                break;
+            case BlockColor.Black:
+                matColor = Color.black;
+                break;
+            default:
+                //Debug color
+                matColor = Color.cyan;
+                break;
+        }
+        Material mat = new Material(Shader.Find("Standard"));
+
+        mat.SetColor("_Color", matColor);
+
+        GetComponent<Renderer>().material = mat;
+    }
+}
